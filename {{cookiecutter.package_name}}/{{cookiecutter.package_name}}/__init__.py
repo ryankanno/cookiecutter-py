71dd5c924bbc6904b4cbd750459280b5b7accff0
Ryan <ryankanno@localkinegrinds.com>
2022-07-25 08:35:11 +0900

    Added flake8/black config

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -7,7 +7,9 @@ __version__ = '{{ cookiecutter.version }}'
 __author__ = '{{ cookiecutter.author_name }}'
 __email__ = '{{ cookiecutter.author_email }}'
 __license__ = '{{ cookiecutter.project_license }}'
-__copyright__ = "Copyright (c) {% now 'utc', '%Y' %}, {{cookiecutter.author_name}}"
+__copyright__ = (
+    "Copyright (c) {% now 'utc', '%Y' %}, {{cookiecutter.author_name}}"
+)
 
 
 # vim: fenc=utf-8
