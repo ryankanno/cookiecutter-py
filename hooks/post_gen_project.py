f852647f53ab98e0212934c2063065a47689d486
Ryan <ryankanno@localkinegrinds.com>
2022-08-06 12:59:00 +0900

    feat: added boolean to allow disabling of github dependabot / actions

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,23 +5,49 @@
 #
 # Distributed under terms of the MIT license.
 
-import os
+from pathlib import Path
 from distutils.util import strtobool
 
-PROJECT_ROOT_DIR = os.path.realpath(os.path.curdir)
+
+PROJECT_ROOT_DIR = Path.cwd().absolute()
+PROJECT_GITHUB = PROJECT_ROOT_DIR / Path(".github")
 
 
-def remove_file(fp):
-    os.remove(os.path.join(PROJECT_ROOT_DIR, fp))
+def remove_path(path: Path) -> None:
+    if path.is_file() or path.is_symlink():
+        path.unlink()
+        return
+    for p in path.iterdir():
+        remove_path(p)
+    path.rmdir()
 
 
-def manage_author_files(should_create_author_files):
+def manage_author_files(should_create_author_files: str) -> None:
     if not strtobool(should_create_author_files):
-        remove_file('AUTHORS.rst')
+        remove_path(PROJECT_ROOT_DIR / Path("AUTHORS.rst"))
+
+
+def manage_github_files(
+    should_install_github_dependabot: str, should_install_github_actions: str
+) -> None:
+
+    if not strtobool(should_install_github_dependabot):
+        remove_path(PROJECT_GITHUB / Path("dependabot.yml"))
+
+    if not strtobool(should_install_github_actions):
+        remove_path(PROJECT_GITHUB / Path("labeler.yml"))
+        remove_path(PROJECT_GITHUB / Path("workflows"))
+
+    if not any(Path(PROJECT_GITHUB).iterdir()):
+        remove_path(PROJECT_GITHUB)
 
 
 if __name__ == '__main__':
     manage_author_files('{{ cookiecutter.should_create_author_files }}')
+    manage_github_files(
+        '{{ cookiecutter.should_install_github_dependabot }}',
+        '{{ cookiecutter.should_install_github_actions}}',
+    )
 
 
 # vim: fenc=utf-8
--- tests/test_cookiecutter_generation.py
+++ tests/test_cookiecutter_generation.py
@@ -5,6 +5,7 @@ Tests project generation
 import os
 import re
 
+from distutils.util import strtobool
 from typing import List
 
 from binaryornot.check import is_binary
@@ -15,47 +16,58 @@ PATTERN = r"{{(\s?cookiecutter)[.](.*?)}}"
 RE_OBJ = re.compile(PATTERN)
 
 
-EXPECTED_BAKED_PROJECT_FILES = [
+EXPECTED_BASE_BAKED_PROJECT_FILES = [
     '.commitlint.config.js',
     '.coveragerc',
     '.dockerignore',
     '.flake8',
-    '.github/dependabot.yml',
-    '.github/labeler.yml',
-    '.github/workflows/ci.yml',
-    '.github/workflows/codeql-analysis.yml',
-    '.github/workflows/commitlint.yml',
-    '.github/workflows/hadolint.yml',
-    '.github/workflows/pr-labeler.yml',
-    '.github/workflows/pr-size-labeler.yml',
     '.gitignore',
     '.konchrc',
     '.pre-commit-config.yaml',
-    'AUTHORS.rst',
     'Dockerfile',
     'HISTORY.rst',
     'LICENSE',
     'Makefile',
     'README.rst',
-    'docs/Makefile',
-    'docs/conf.py',
-    'docs/index.rst',
-    'docs/make.bat',
+    '/docs/Makefile',
+    '/docs/conf.py',
+    '/docs/index.rst',
+    '/docs/make.bat',
     'logging.yaml',
     'pyproject.toml',
     'pytest.ini',
     'setup.cfg',
-    'test_project/__init__.py',
-    'test_project/test_project.py',
-    'tests/test_test_project.py',
+    '/test_project/__init__.py',
+    '/test_project/test_project.py',
+    '/tests/test_test_project.py',
     'tox.ini',
 ]
 
+EXPECTED_BAKED_AUTHORS_FILES = [
+    'AUTHORS.rst',
+]
 
-def build_files_list(root_dir):
-    """Build a list containing absolute paths to the generated files."""
+EXPECTED_BAKED_GITHUB_DEPENDABOT_FILES = [
+    '/.github/dependabot.yml',
+]
+
+EXPECTED_BAKED_GITHUB_ACTIONS_FILES = [
+    '/.github/labeler.yml',
+    '/.github/workflows/ci.yml',
+    '/.github/workflows/codeql-analysis.yml',
+    '/.github/workflows/commitlint.yml',
+    '/.github/workflows/hadolint.yml',
+    '/.github/workflows/pr-labeler.yml',
+    '/.github/workflows/pr-size-labeler.yml',
+]
+
+
+def build_files_list(root_dir: str, is_absolute: bool = True) -> List[str]:
+    """Build a list containing abs/relative paths to the generated files."""
     return [
         os.path.join(dirpath, file_path)
+        if is_absolute
+        else os.path.join(dirpath[len(root_dir) :], file_path)
         for dirpath, subdirs, files in os.walk(root_dir)
         for file_path in files
     ]
@@ -76,8 +88,9 @@ def check_paths_exist(
     expected_paths: List[str], baked_files: List[str]
 ) -> None:
     assert len(expected_paths) == len(baked_files)
-    for i, baked_file in enumerate(baked_files):
-        assert expected_paths[i] in baked_file
+
+    for _, expected_path in enumerate(expected_paths):
+        assert expected_path in baked_files
 
 
 def test_with_default_configuration(cookies, context):
@@ -88,10 +101,73 @@ def test_with_default_configuration(cookies, context):
     assert baked_project.exception is None
     assert baked_project.project_path.is_dir()
 
-    baked_files = build_files_list(str(baked_project.project_path))
-    baked_files.sort()
+    abs_baked_files = build_files_list(str(baked_project.project_path))
+    assert abs_baked_files
+    check_paths_substitution(abs_baked_files)
 
-    assert baked_files
+    rel_baked_files = build_files_list(
+        str(baked_project.project_path), is_absolute=False
+    )
+    assert rel_baked_files
 
-    check_paths_substitution(baked_files)
-    check_paths_exist(EXPECTED_BAKED_PROJECT_FILES, baked_files)
+    if (
+        strtobool(context['should_create_author_files'])
+        and strtobool(context['should_install_github_dependabot'])
+        and strtobool(context['should_install_github_actions'])
+    ):
+        check_paths_exist(
+            EXPECTED_BASE_BAKED_PROJECT_FILES
+            + EXPECTED_BAKED_AUTHORS_FILES
+            + EXPECTED_BAKED_GITHUB_DEPENDABOT_FILES
+            + EXPECTED_BAKED_GITHUB_ACTIONS_FILES,
+            rel_baked_files,
+        )
+    elif (
+        strtobool(context['should_create_author_files'])
+        and strtobool(context['should_install_github_dependabot'])
+        and not strtobool(context['should_install_github_actions'])
+    ):
+        check_paths_exist(
+            EXPECTED_BASE_BAKED_PROJECT_FILES
+            + EXPECTED_BAKED_AUTHORS_FILES
+            + EXPECTED_BAKED_GITHUB_DEPENDABOT_FILES,
+            rel_baked_files,
+        )
+    elif (
+        strtobool(context['should_create_author_files'])
+        and not strtobool(context['should_install_github_dependabot'])
+        and strtobool(context['should_install_github_actions'])
+    ):
+        check_paths_exist(
+            EXPECTED_BASE_BAKED_PROJECT_FILES
+            + EXPECTED_BAKED_AUTHORS_FILES
+            + EXPECTED_BAKED_GITHUB_ACTIONS_FILES,
+            rel_baked_files,
+        )
+    elif (
+        not strtobool(context['should_create_author_files'])
+        and strtobool(context['should_install_github_dependabot'])
+        and strtobool(context['should_install_github_actions'])
+    ):
+        check_paths_exist(
+            EXPECTED_BASE_BAKED_PROJECT_FILES
+            + EXPECTED_BAKED_GITHUB_DEPENDABOT_FILES
+            + EXPECTED_BAKED_GITHUB_ACTIONS_FILES,
+            rel_baked_files,
+        )
+    elif (
+        not strtobool(context['should_create_author_files'])
+        and not strtobool(context['should_install_github_dependabot'])
+        and strtobool(context['should_install_github_actions'])
+    ):
+        check_paths_exist(
+            EXPECTED_BASE_BAKED_PROJECT_FILES
+            + EXPECTED_BAKED_GITHUB_ACTIONS_FILES,
+            rel_baked_files,
+        )
+    elif (
+        not strtobool(context['should_create_author_files'])
+        and not strtobool(context['should_install_github_dependabot'])
+        and not strtobool(context['should_install_github_actions'])
+    ):
+        check_paths_exist(EXPECTED_BASE_BAKED_PROJECT_FILES, rel_baked_files)
