d265e6394a405a874424504acbbcd9ff54760215
Ryan Kanno <ryankanno@localkinegrinds.com>
2022-08-06 14:12:39 +0900

    feat: added boolean to allow disabling of github dependabot / actions (#32)

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,22 @@
 import pytest
 
 
-@pytest.fixture()
-def context():
+@pytest.fixture(
+    params=[
+        ('y', 'y', 'y'),
+        ('y', 'y', 'n'),
+        ('y', 'n', 'y'),
+        ('n', 'y', 'y'),
+        ('n', 'n', 'y'),
+        ('n', 'n', 'n'),
+    ]
+)
+def context(request):
+
+    should_create_author_files = request.param[0]
+    should_install_github_dependabot = request.param[1]
+    should_install_github_actions = request.param[2]
+
     """Creates default prompt vals."""
     return {
         'author_name': 'Ryan',
@@ -21,7 +35,9 @@ def context():
         'package_name': 'test_project',
         'version': '0.0.1',
         'python_version': '3.8',
-        'should_create_author_files': 'y',
+        'should_create_author_files': should_create_author_files,
+        'should_install_github_dependabot': should_install_github_dependabot,
+        'should_install_github_actions': should_install_github_actions,
     }
 
 
