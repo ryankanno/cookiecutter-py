1d77cd3e38f155269f58a027a1db6294571aa58f
Ryan <ryankanno@localkinegrinds.com>
2022-08-10 13:14:54 +0900

    cleaned up version output

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from importlib import metadata
+import importlib.metadata
 
 
 __title__ = '{{ cookiecutter.project_name }}'
-__version__ = metadata.version(__package__)
+__version__ = importlib.metadata.version(__package__ or __name__)
 __author__ = '{{ cookiecutter.author_name }}'
 __email__ = '{{ cookiecutter.author_email }}'
 __license__ = '{{ cookiecutter.project_license }}'
 # fmt: off
 __copyright__ = "Copyright (c) {% now 'utc', '%Y' %}, {{cookiecutter.author_name}}"
 # fmt: on
-
-del metadata
 
 
 # vim: fenc=utf-8
