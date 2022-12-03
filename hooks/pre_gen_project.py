858e8ded9a3492526c94659b26cd7f40f1e0fbf4
Ryan <ryankanno@localkinegrinds.com>
2022-12-04 05:56:44 +0900

    feat: asupport dynamic supported python versions

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2020 Ryan Kanno <ryankanno@localkinegrinds.com>
+#
+# Distributed under terms of the MIT license.
+
+import sys
+
+
+VALID_PYTHON_VERSION_PREFIXES = [
+    "3.7", "3.8", "3.9", "3.10", "3.11",
+    "pypy3.7", "pypy3.8", "pypy3.9"
+]
+
+supported_python_versions = '{{ cookiecutter.supported_python_versions }}'
+
+supported_python_versions_list = [
+    py_version.strip() for py_version in supported_python_versions.split(',')
+]
+
+for version in supported_python_versions_list:
+    if not version.startswith(tuple(VALID_PYTHON_VERSION_PREFIXES)):
+        print(f"ERROR: {version} is not a valid supported Python version. Supported are any Pythons that begin with the following prefixes: {VALID_PYTHON_VERSION_PREFIXES}")
+        sys.exit(1)
+
+
+# vim: fenc=utf-8
+# vim: filetype=python
