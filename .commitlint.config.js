c5fe569af94414262c92f27bb3af6f5cb02e8121
Ryan <ryankanno@localkinegrinds.com>
2023-11-03 13:08:04 +0900

    updated commitlint

--- .commitlint.config.js
+++ .commitlint.config.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     // dependabot
     'header-max-length': [2, 'always', 200],
-    'body-max-line-length': [0, 'always']
+    'body-max-line-length': [0, 'always'],
+    'subject-case': [2, 'always', ['sentence-case', 'lower-case']]
   },
 };
