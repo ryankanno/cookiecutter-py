a7bb6575e3a4a4e891d0b02baeb458a533b52ab0
Ryan Kanno <ryankanno@localkinegrinds.com>
2024-06-02 05:59:52 -1000

    fix: added doc modules (#420)
    - generate modules.rst into docs/sources
    - removed -W flag from sphinx generation

--- {{cookiecutter.package_name}}/docs/conf.py
+++ {{cookiecutter.package_name}}/docs/conf.py
@@ -3,11 +3,17 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath('../{{cookiecutter.package_name}}/'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = '{{cookiecutter.package_name}}'
-copyright = '2024, {{cookiecutter.author_name}}'
+project_copyright = '2024, {{cookiecutter.author_name}}'
 author = '{{cookiecutter.author_name}}'
 
 # -- General configuration ---------------------------------------------------
